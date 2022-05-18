725f47324349f1d5a1d633832a81cf8cf2c9dcc6
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-05-18 16:58:18 -0400

    Work on the dialogue

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class Engineer extends Employee {
 }
 class Intern extends Employee {
     constructor(name, employee_ID, e_mail_address, school_name) {
-        super(name, employee_ID, e - mail_address);
+        super(name, employee_ID, e_mail_address);
         this.school = school_name;
     }
     getSchool() { return this.school };
@@ -70,7 +70,7 @@ const manager_questions = [
     {
         type: "input",
         name: "manager_employee_ID",
-        message: "What is the manager's employee ID",
+        message: "What is the manager's employee ID?",
         default: "",
         validate: function (response) {
             if (response == "") {
@@ -105,9 +105,9 @@ inquirer.prompt(manager_questions)
 
 function process_manager_answers(answers) {
     /* Create our one and only manager.  */
-    const the_manager = new Manager(answers.manager_name, 
+    const the_manager = new Manager(answers.manager_name,
         answers.manager_employee_ID,
-        answers.manager_e_mail_address, 
+        answers.manager_e_mail_address,
         answers.manager_office_number);
     team_members.push(the_manager);
     add_to_team();
@@ -116,14 +116,15 @@ function process_manager_answers(answers) {
 /* Add to the team until the user says he is done.
  * After adding an Engineer or Intern, we come back here.
  */
-function add_to_team () {
+function add_to_team() {
 
-    /* Ask the user what he woudl like to do.  */
+    /* Ask the user what he would like to do.  */
     const choice_questions = [
         {
             type: "list",
             name: "choice",
-            message: "Add an Engineer or Intern to the team, or team is complete",
+            message: "Would you like to add an Engineer or Intern " + 
+                "to the team, or is the team complete?",
             choices: ["add an Engineer", "add an Intern", "team is complete"],
             default: "team is complete"
         }
@@ -139,7 +140,7 @@ function process_choice_answer(answer) {
             add_an_Engineer();
             break;
         case "add an Intern":
-            add_an_Intern();
+            add_an_intern();
             break;
         case "team is complete":
             complete_team();
@@ -151,15 +152,137 @@ function process_choice_answer(answer) {
 
 /* The user wishes to add an Engineer to the team.  */
 function add_an_Engineer() {
+    /* The Engineer dialogue asks about name, employee ID, e-mail
+     * address and GitHub user name.
+     */
+    const engineer_questions = [
+        {
+            type: "input",
+            name: "engineer_name",
+            message: "What is the Engineer's name?",
+            default: "",
+            validate: function (response) {
+                if (response == "") {
+                    return "You must provide the name of the Engineer.";
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "engineer_employee_ID",
+            message: "What is the Engineer's employee ID?",
+            default: "",
+            validate: function (response) {
+                if (response == "") {
+                    return "You must provide the employee ID of the Engineer.";
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "engineer_e_mail_address",
+            message: "What is the Engineer's e-mail address?",
+            default: "",
+            validate: function (response) {
+                if (response == "") {
+                    return "You must provide the e-mail address of the Engineer.";
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "engineer_github_user_name",
+            message: "What is the Engineer's GitHub user name?",
+            default: "",
+        }
+    ];
+
+    /* Get the answers to the Engineer questions.  */
+    inquirer.prompt(engineer_questions)
+        .then(answers => process_engineer_answers(answers));
+}
+
+function process_engineer_answers(answers) {
+    /* Add this Engineer to the team.  */
+    const the_engineer = new Engineer(answers.engineer_name,
+        answers.engineer_employee_ID,
+        answers.engineer_e_mail_address,
+        answers.engineer_github_user_name);
+    team_members.push(the_engineer);
     add_to_team();
 }
 
-/* The user wishes to add an Intern to the team.  */
-function add_an_Intern () {
+/* The user wishes to add an intern to the team.  */
+function add_an_intern() {
+    /* The Intern dialogue asks about name, employee ID, e-mail
+     * address and school name.
+     */
+    const intern_questions = [
+        {
+            type: "input",
+            name: "intern_name",
+            message: "What is the intern's name?",
+            default: "",
+            validate: function (response) {
+                if (response == "") {
+                    return "You must provide the name of the intern.";
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "intern_employee_ID",
+            message: "What is the intern's employee ID?",
+            default: "",
+            validate: function (response) {
+                if (response == "") {
+                    return "You must provide the employee ID of the intern.";
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "intern_e_mail_address",
+            message: "What is the intern's e-mail address?",
+            default: "",
+            validate: function (response) {
+                if (response == "") {
+                    return "You must provide the e-mail address of the intern.";
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "intern_school_name",
+            message: "What is the name of the intern's school?",
+            default: "",
+        }
+    ];
+
+    /* Get the answers to the Engineer questions.  */
+    inquirer.prompt(intern_questions)
+        .then(answers => process_intern_answers(answers));
+}
+
+function process_intern_answers(answers) {
+    /* Add this intern to the team.  */
+    const the_intern = new Intern(answers.intern_name,
+        answers.intern_employee_ID,
+        answers.intern_e_mail_address,
+        answers.intern_school_name);
+    team_members.push(the_intern);
     add_to_team();
 }
+
 
 /* The team is complete.  */
-function complete_team () {
-    console.log ("team is complete");
+function complete_team() {
+    console.log("team is complete");
+    console.log(team_members);
 }
